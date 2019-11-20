e079eb42e765a9c04b0eb04a36938f0ff77c3b0e
notedit <notedit@gmail.com>
2019-11-20 16:20:43 +0800

    fix publish bench

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/notedit/webrtc-bench
 
 require (
-	github.com/imroc/req v0.2.3 // indirect
-	github.com/pion/webrtc/v2 v2.0.15 // indirect
+	github.com/imroc/req v0.2.3
+	github.com/notedit/gstreamer-go v0.3.1 // indirect
+	github.com/pion/webrtc/v2 v2.1.3-0.20190811215351-270cd825b407
 )
--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	//"encoding/json"
+	"math/rand"
 	"github.com/pion/webrtc/v2"
 	"github.com/imroc/req"
+	gstreamer "github.com/notedit/gstreamer-go"
 )
 
 
-var streamID = "dn3V99g1U0S8m6Z7pjnbxAHo3jpSBD4ne0AR"
 
-
-func main() {
+func benchPlay() {
+	
+	
+	var streamID = "v7Vj09vH3DIaeS84S5ctDmJGgRsVDVOyzEyN"
 
 	config := webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
@@ -55,7 +57,7 @@ func main() {
 	
 		peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
 
-			//fmt.Printf("Connection State has changed %s \n", connectionState.String())
+			fmt.Printf("Connection State has changed %s \n", connectionState.String())
 		})
 	
 	
@@ -72,7 +74,7 @@ func main() {
 		}
 	
 	
-		res, err := req.Post("http://127.0.0.1:6000/api/play", req.BodyJSON(map[string]string{
+		res, err := req.Post("http://127.0.0.1:6001/api/play", req.BodyJSON(map[string]string{
 			"streamId": streamID,
 			"sdp":      offer.SDP,
 		}))
@@ -82,7 +84,7 @@ func main() {
 		}
 	
 		var ret struct {
-			Status int               `json:"s"`
+			Status int      `json:"s"`
 			Data   struct {
 				Sdp string  `json:"sdp"`
 			} `json:"d"`
@@ -111,4 +113,134 @@ func main() {
 
 
 	select {}
+
+}
+
+
+func benchPublish() {
+
+	var rtmpSource = "rtmp://alihb2clive.wangxiao.eaydu.com/live_ali/x_3_test_ali"
+
+
+	pipelineStr := "gst-launch-1.0 -v rtmpsrc location=%s ! flvdemux name=demux  demux.video ! queue ! h264parse ! rtph264pay timestamp-offset=0 config-interval=-1 ! appsink name=sink"
+
+	pipelineStr = fmt.Sprintf(pipelineStr, rtmpSource)
+
+	pipeline, err := gstreamer.New(pipelineStr)
+	if err != nil {
+		panic(err)
+	}
+	
+	appsink := pipeline.FindElement("sink")
+	
+	pipeline.Start()
+	
+	out := appsink.Poll()
+
+
+
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+		BundlePolicy: webrtc.BundlePolicyMaxBundle,
+		SDPSemantics: webrtc.SDPSemanticsPlanB,
+	}
+
+	m := webrtc.MediaEngine{}
+	m.RegisterCodec(webrtc.NewRTPOpusCodec(webrtc.DefaultPayloadTypeOpus, 48000))
+	m.RegisterCodec(webrtc.NewRTPH264Codec(webrtc.DefaultPayloadTypeH264, 90000))
+	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
+
+
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
+
+	peerConnection, err := api.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+	
+	// Create a video track
+	videoTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeH264, rand.Uint32(), "video", "video")
+	if err != nil {
+		panic(err)
+	}
+	_, err = peerConnection.AddTrack(videoTrack)
+	if err != nil {
+		panic(err)
+	}
+
+
+	peerConnection.OnTrack(func(track *webrtc.Track, receiver *webrtc.RTPReceiver){
+
+	})
+
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	})
+
+
+	offer, err := peerConnection.CreateOffer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+
+	err = peerConnection.SetLocalDescription(offer)
+	if err != nil {
+		panic(err)
+	}
+
+
+	res, err := req.Post("http://127.0.0.1:6001/api/publish", req.BodyJSON(map[string]string{
+		"streamId": "streamdddddddd",
+		"sdp":      offer.SDP,
+	}))
+
+	if err != nil {
+		panic(err)
+	}
+
+	var ret struct {
+		Status int      `json:"s"`
+		Data   struct {
+			Sdp string  `json:"sdp"`
+		} `json:"d"`
+	}
+	
+	err = res.ToJSON(&ret)
+	if err != nil {
+		panic(err)
+	}
+
+	answerStr := ret.Data.Sdp
+
+	answer := webrtc.SessionDescription{
+		SDP: answerStr,
+		Type: webrtc.SDPTypeAnswer,
+	}
+
+	err = peerConnection.SetRemoteDescription(answer)
+
+	if err != nil {
+		panic(err)
+	}
+
+
+	for {
+		buffer := <-out
+
+		videoTrack.Write(buffer)
+		fmt.Println("push ", len(buffer))
+	}
+	
+}
+
+
+func main() {
+
+	benchPublish()
 }
