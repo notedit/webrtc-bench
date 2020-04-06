67416c7a6eede475c3c4bcfa99b8fed20b722425
notedit <notedit@gmail.com>
2020-04-06 21:57:00 +0800

    fix webrtc bench

--- go.mod
+++ go.mod
@@ -1,7 +1,17 @@
 module github.com/notedit/webrtc-bench
 
 require (
+	github.com/alexflint/go-arg v1.3.0 // indirect
+	github.com/golang/protobuf v1.3.5 // indirect
 	github.com/imroc/req v0.2.3
-	github.com/notedit/gstreamer-go v0.3.1 // indirect
-	github.com/pion/webrtc/v2 v2.1.3-0.20190811215351-270cd825b407
+	github.com/lucas-clemente/quic-go v0.15.2 // indirect
+	github.com/pion/ice v0.7.12 // indirect
+	github.com/pion/rtp v1.4.0 // indirect
+	github.com/pion/sdp/v2 v2.3.5 // indirect
+	github.com/pion/webrtc/v2 v2.2.4 // indirect
+	golang.org/x/crypto v0.0.0-20200403201458-baeed622b8d8 // indirect
+	golang.org/x/sys v0.0.0-20200406113430-c6e801f48ba2 // indirect
+
 )
+
+go 1.13
--- main.go
+++ main.go
@@ -2,245 +2,104 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"github.com/pion/webrtc/v2"
+	"github.com/alexflint/go-arg"
 	"github.com/imroc/req"
-	gstreamer "github.com/notedit/gstreamer-go"
+	"github.com/pion/webrtc/v2"
 )
 
+func benchPlay(streamURL string, apiURL string, count int) {
 
-
-func benchPlay() {
-	
-	
-	var streamID = "v7Vj09vH3DIaeS84S5ctDmJGgRsVDVOyzEyN"
 
 	config := webrtc.Configuration{
-		ICEServers: []webrtc.ICEServer{
-			{
-				URLs: []string{"stun:stun.l.google.com:19302"},
-			},
-		},
+		ICEServers:   []webrtc.ICEServer{},
 		BundlePolicy: webrtc.BundlePolicyMaxBundle,
-		SDPSemantics: webrtc.SDPSemanticsPlanB,
+		SDPSemantics: webrtc.SDPSemanticsUnifiedPlan,
 	}
-
 
 	m := webrtc.MediaEngine{}
 	m.RegisterCodec(webrtc.NewRTPOpusCodec(webrtc.DefaultPayloadTypeOpus, 48000))
 	m.RegisterCodec(webrtc.NewRTPH264Codec(webrtc.DefaultPayloadTypeH264, 90000))
-	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
-	
+
 	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
 
-
-	for i := 0; i < 10; i++ {
+	for i := 0; i < count; i++ {
 
 		peerConnection, err := api.NewPeerConnection(config)
 		if err != nil {
 			panic(err)
 		}
-	
-	
-		// add audio and video tranceiver 
-		if _, err = peerConnection.AddTransceiver(webrtc.RTPCodecTypeAudio, webrtc.RtpTransceiverInit{Direction: webrtc.RTPTransceiverDirectionRecvonly}); err != nil {
+
+		if _, err = peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio, webrtc.RtpTransceiverInit{Direction: webrtc.RTPTransceiverDirectionRecvonly}); err != nil {
 			panic(err)
-		} else if _, err = peerConnection.AddTransceiver(webrtc.RTPCodecTypeVideo,webrtc.RtpTransceiverInit{Direction: webrtc.RTPTransceiverDirectionRecvonly}); err != nil {
+		} else if _, err = peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeVideo, webrtc.RtpTransceiverInit{Direction: webrtc.RTPTransceiverDirectionRecvonly}); err != nil {
 			panic(err)
 		}
 
-		peerConnection.OnTrack(func(track *webrtc.Track, receiver *webrtc.RTPReceiver){
-
-			codec := track.Codec()
-			fmt.Println("Track has started", i,  codec.Name)
-		})
-	
 		peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-
-			fmt.Printf("Connection State has changed %s \n", connectionState.String())
+			fmt.Printf("Connection State has changed %s  connection id %d \n", connectionState.String(), i)
 		})
-	
-	
-	
+
 		offer, err := peerConnection.CreateOffer(nil)
 		if err != nil {
 			panic(err)
 		}
 
-
 		err = peerConnection.SetLocalDescription(offer)
 		if err != nil {
 			panic(err)
 		}
-	
-	
-		res, err := req.Post("http://127.0.0.1:6001/api/play", req.BodyJSON(map[string]string{
-			"streamId": streamID,
+
+		res, err := req.Post(apiURL, req.BodyJSON(map[string]string{
+			"streamurl": streamURL,
 			"sdp":      offer.SDP,
 		}))
-	
+
 		if err != nil {
 			panic(err)
 		}
-	
+
 		var ret struct {
-			Status int      `json:"s"`
-			Data   struct {
-				Sdp string  `json:"sdp"`
-			} `json:"d"`
+			Code int
+			Sdp  string
 		}
-		
+
 		err = res.ToJSON(&ret)
 		if err != nil {
 			panic(err)
 		}
-	
-		answerStr := ret.Data.Sdp
-	
+
+		answerStr := ret.Sdp
+
 		answer := webrtc.SessionDescription{
-			SDP: answerStr,
+			SDP:  answerStr,
 			Type: webrtc.SDPTypeAnswer,
 		}
-	
-	
+
 		err = peerConnection.SetRemoteDescription(answer)
-	
+
 		if err != nil {
 			panic(err)
 		}
 
 	}
-
 
 	select {}
 
 }
 
-
-func benchPublish() {
-
-	var rtmpSource = "rtmp://alihb2clive.wangxiao.eaydu.com/live_ali/x_3_test_ali"
-
-
-	pipelineStr := "gst-launch-1.0 -v rtmpsrc location=%s ! flvdemux name=demux  demux.video ! queue ! h264parse ! rtph264pay timestamp-offset=0 config-interval=-1 ! appsink name=sink"
-
-	pipelineStr = fmt.Sprintf(pipelineStr, rtmpSource)
-
-	pipeline, err := gstreamer.New(pipelineStr)
-	if err != nil {
-		panic(err)
-	}
-	
-	appsink := pipeline.FindElement("sink")
-	
-	pipeline.Start()
-	
-	out := appsink.Poll()
-
-
-
-	config := webrtc.Configuration{
-		ICEServers: []webrtc.ICEServer{
-			{
-				URLs: []string{"stun:stun.l.google.com:19302"},
-			},
-		},
-		BundlePolicy: webrtc.BundlePolicyMaxBundle,
-		SDPSemantics: webrtc.SDPSemanticsPlanB,
-	}
-
-	m := webrtc.MediaEngine{}
-	m.RegisterCodec(webrtc.NewRTPOpusCodec(webrtc.DefaultPayloadTypeOpus, 48000))
-	m.RegisterCodec(webrtc.NewRTPH264Codec(webrtc.DefaultPayloadTypeH264, 90000))
-	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
-
-
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
-
-	peerConnection, err := api.NewPeerConnection(config)
-	if err != nil {
-		panic(err)
-	}
-	
-	// Create a video track
-	videoTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeH264, rand.Uint32(), "video", "video")
-	if err != nil {
-		panic(err)
-	}
-	_, err = peerConnection.AddTrack(videoTrack)
-	if err != nil {
-		panic(err)
-	}
-
-
-	peerConnection.OnTrack(func(track *webrtc.Track, receiver *webrtc.RTPReceiver){
-
-	})
-
-	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-
-		fmt.Printf("Connection State has changed %s \n", connectionState.String())
-	})
-
-
-	offer, err := peerConnection.CreateOffer(nil)
-	if err != nil {
-		panic(err)
-	}
-
-
-	err = peerConnection.SetLocalDescription(offer)
-	if err != nil {
-		panic(err)
-	}
-
-
-	res, err := req.Post("http://127.0.0.1:6001/api/publish", req.BodyJSON(map[string]string{
-		"streamId": "streamdddddddd",
-		"sdp":      offer.SDP,
-	}))
-
-	if err != nil {
-		panic(err)
-	}
-
-	var ret struct {
-		Status int      `json:"s"`
-		Data   struct {
-			Sdp string  `json:"sdp"`
-		} `json:"d"`
-	}
-	
-	err = res.ToJSON(&ret)
-	if err != nil {
-		panic(err)
-	}
-
-	answerStr := ret.Data.Sdp
-
-	answer := webrtc.SessionDescription{
-		SDP: answerStr,
-		Type: webrtc.SDPTypeAnswer,
-	}
-
-	err = peerConnection.SetRemoteDescription(answer)
-
-	if err != nil {
-		panic(err)
-	}
-
-
-	for {
-		buffer := <-out
-
-		videoTrack.Write(buffer)
-		fmt.Println("push ", len(buffer))
-	}
-	
-}
-
-
 func main() {
 
-	benchPublish()
+	var args struct {
+		Count  int    `arg:"required" "-c" help:"stream count to play"`
+		Stream string `arg:"required" "-s" help:"stream url"`
+		Url    string `arg:"required" "-u" help:"http url to play"`
+	}
+
+	err := arg.Parse(&args)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	benchPlay(args.Stream, args.Url,args.Count)
 }
